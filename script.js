540bf0ac6aabe9cfc47d4549c9d0026dfefeda8c
nishb369 <nishbcodes@gmail.com>
2024-10-21 19:41:10 +0530

    btn functionality added

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ gsap.to(box, {
         // pinSpacing: false, // This prevents the creation of extra space
     }
 });
+
+const aurDekhen = document.querySelector('.aur-dekhen');
+const aurDekhenCircle = document.querySelector('.aur-dekhen-circle');
+
+gsap.set(aurDekhenCircle, { scale: 1 });
+
+aurDekhen.addEventListener('mouseenter', () => {
+    gsap.to(aurDekhen, {
+        backgroundColor: 'black',
+        borderColor: '#ffa500',
+        borderWidth: 2,
+        duration: 0.5,
+        ease: 'power2.inOut'
+    });
+    
+    gsap.to(aurDekhenCircle, {
+        scale: 1.2,
+        borderColor: '#ffa500',
+        borderWidth: 2,
+        duration: 0.5,
+        ease: 'power2.inOut'
+    });
+});
+
+aurDekhen.addEventListener('mouseleave', () => {
+    gsap.to(aurDekhen, {
+        backgroundColor: '#ffa500',
+        borderColor: 'black',
+        borderWidth: 1,
+        duration: 0.5,
+        ease: 'power2.inOut'
+    });
+    
+    gsap.to(aurDekhenCircle, {
+        scale: 1,
+        borderColor: 'black',
+        borderWidth: 1,
+        duration: 0.5,
+        ease: 'power2.inOut'
+    });
+});
